d94d5eea84d1522de82b137b1d1b48e1367b7a82
DrRon <piepmail@lefthandmedia.com>
2023-06-19 12:08:10 +0200

    new ppv2

---
+++ ProjectPlayStars/Assets/Scripts/WinManager.cs
@@ -0,0 +1,74 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class WinManager : MonoBehaviour
+{
+	[SerializeField] private StarGeneratorData strData;
+	[SerializeField] private GameObject feedbackImage;
+	[SerializeField] private SpriteRenderer feedbackRndr;
+	[SerializeField] private MiniMapMaker MiMaMa;
+    [SerializeField] private AudioSource auditS;
+    [SerializeField] private List<Sprite> sprites = new List<Sprite>();
+
+	private void Start()
+	{
+        feedbackRndr.enabled = false;
+        feedbackRndr.enabled = false;
+        feedbackRndr.enabled = false;
+    }
+
+	public void CheckIfWon()
+    {
+        auditS.Play();
+        int counter = 0;
+
+        foreach (GameObject realProbe in strData.RealProbesList)
+        {
+            if (realProbe.GetComponent<RealProbes>().Captured)
+            {
+                counter++;
+            }
+            else
+            {
+                return;
+            }
+        }
+
+        if (counter == strData.RealProbesList.Count)
+        {
+            Won();
+        }
+    }
+
+    public void MiniMapUpdate(GameObject target)
+	{
+        for (int i = 0; i < strData.RealProbesList.Count; i++)
+		{
+            if (strData.RealProbesList[i] == target)
+			{
+                MiMaMa.realProbes[i].SetActive(false);
+            }
+		}
+	}
+
+    public void Feedback(int feedbackIndex)
+	{
+        feedbackRndr.sprite = sprites[feedbackIndex];
+        feedbackImage.GetComponent<Animator>().Play("feedback");
+        Debug.Log(feedbackIndex);
+    }
+
+    private void Won()
+    {
+        Debug.Log("game won");
+        StartCoroutine(waitabit());
+    }
+
+    IEnumerator waitabit()
+	{
+        yield return new WaitForSeconds(2);
+        SceneManager.LoadScene("Win");
+    }
+}
